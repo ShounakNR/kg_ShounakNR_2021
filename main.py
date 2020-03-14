2d1a6eb9c35b7f071ee0f5e46e5656e000e95ec4
Shounak Rangwala <55153887+ShounakNR@users.noreply.github.com>
2020-03-14 19:02:07 -0400

    Initial commit of py main script
    Fully functional code for one to one mapping of the input strings

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+def OneToOne ():    
+    S1 = sys.argv[1]
+    S2 = sys.argv[2]       
+    if len(S1)!=len(S2):
+        return False
+    
+    Dict1={}
+    for i in range(len(S1)):
+        if  S1[i] not in Dict1.keys():
+            Dict1[S1[i]]=S2[i]
+        else:
+            if Dict1[S1[i]]!=S2[i]:
+                return False
+    return True
+print(OneToOne())
